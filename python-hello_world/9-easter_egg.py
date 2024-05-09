e1dc9efbee781518c23bc720330d825d6855d9ec
rufatjabbarov22 <rufatjabbarov001@gmail.com>
2024-05-09 13:17:48 +0000

    solution

--- python-hello_world/9-easter_egg.py
+++ python-hello_world/9-easter_egg.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 print("The Zen of Python, by Tim Peters
-
-Beautiful is better than ugly.
+        Beautiful is better than ugly.
 Explicit is better than implicit.
 Simple is better than complex.
 Complex is better than complicated.
@@ -20,3 +19,4 @@ Although never is often better than *right* now.
 If the implementation is hard to explain, it's a bad idea.
 If the implementation is easy to explain, it may be a good idea.
 Namespaces are one honking great idea -- let's do more of those!")
+
