e5a999d5509aa3044a401b87aec8946c98ed2660
rufatjabbarov22 <rufatjabbarov001@gmail.com>
2024-05-12 11:30:20 +0000

    update

--- python-if_else_loops_functions/9-print_last_digit.py
+++ python-if_else_loops_functions/9-print_last_digit.py
@@ -3,3 +3,8 @@ def print_last_digit(number):
     last_digit = abd(number) % 10
     print(last_digit)
     return last_digit
+
+print_last_digit(98)
+print_last_digit(0)
+r = print_last_digit(-1024)
+print(r)
