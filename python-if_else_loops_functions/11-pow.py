54eb1b40922a4430702c932d5611954506b80d14
rufatjabbarov22 <rufatjabbarov001@gmail.com>
2024-05-13 11:52:35 +0000

    POw

--- python-if_else_loops_functions/11-pow.py
+++ python-if_else_loops_functions/11-pow.py
@@ -1,9 +1,3 @@
 #!/usr/bin/python3
 def pow(a, b):
-    if b >= 0:
-        result = 1
-        for _ in range(b):
-            result *= a
-    else:
-        result = 1.0 / pow(a, -b)
-    return result
+    return a ** b
