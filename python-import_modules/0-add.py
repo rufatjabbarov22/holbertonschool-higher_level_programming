62cd0c67d1dadaf6b756b4e985e351fe2008dbc5
rufatjabbarov22 <rufatjabbarov001@gmail.com>
2024-05-15 10:18:51 +0000

    0

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,16 +1,9 @@
 #!/usr/bin/python3
+a = 1
+b = 2
+
 from add_0 import add
 
+result = add(a, b)
 
-def main():
-    a = 1
-    b = 2
-
-
-    result = add(a + b)
-    print("{} + {} = {}".format(a, b, result))
-
-
-
-if __name__== "__main__":
-    main()
+print("{} + {} = {}".format(a, b, result))
