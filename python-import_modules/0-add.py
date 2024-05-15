6aa67d44721140bfbc943f5f30281c2fba72f2df
rufatjabbarov22 <rufatjabbarov001@gmail.com>
2024-05-15 10:24:02 +0000

    0

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,11 +1,8 @@
 #!/usr/bin/python3
+from add_0 import add
 
 a = 1
 b = 2
 
-from add_0 import add as FAKE_add
-
-result = FAKE_add(a, b)
-
-print("{} + {} = {}".format(a, b, result))
-
+if __name__ == "__main__":
+    print("{} + {} = {}".format(a, b, add(a, b)))
