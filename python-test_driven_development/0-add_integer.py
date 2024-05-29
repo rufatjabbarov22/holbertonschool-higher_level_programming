9e4e228c89a172e9455db4062fb63968ecbcecde
rufatjabbarov22 <rufatjabbarov001@gmail.com>
2024-05-29 16:33:41 +0000

    corection

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,17 +1,15 @@
 #!/usr/bin/python3
+"""0-add_integer.py and tests/0-add_integer.txt"""
 
 
 def add_integer(a, b=98):
     """
-    Return the addition of two integer
-
-    a (int, float) the first number
-    b (int, float) the second number default is 98
-    Computes the sum of two integers
-        return error if a or b is not integer
+    adds an integer
+    unit tests located in tests/0-add_integer.txt
+    checks for type errors
     """
     if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")
-    if not isinstance(b, (int, float)):
+    elif not isinstance(b, (int, float)):
         raise TypeError("b must be an integer")
     return int(a) + int(b)
