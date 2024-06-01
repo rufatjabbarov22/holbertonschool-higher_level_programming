920c03e774e0d2accddd9d03da20a8c14a1f629a
Rufat Jabbarov <152645357+rufatjabbarov22@users.noreply.github.com>
2024-06-01 16:08:54 +0400

    Create 9-rectangle.py

---
+++ python-inheritance/9-rectangle.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+"""Rectangle class"""
+
+
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """Rectangle inheritd BaseGeometry"""
+    def __init__(self, width, height):
+        """Define function"""
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
+
+    def area(self):
+        """Return area of rectangle"""
+        return self.__width * self.__height
+
+    def __str__(self):
+        return ("[Rectangle] {}/{}".format(self.__width, self.__height))
