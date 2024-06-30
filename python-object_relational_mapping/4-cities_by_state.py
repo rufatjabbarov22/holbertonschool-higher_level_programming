519cc8788313db6468a3eec12e3ed1777f1639c1
Rufat Jabbarov <152645357+rufatjabbarov22@users.noreply.github.com>
2024-06-30 19:11:46 +0400

    4-cities_by_state.py

--- python-object_relational_mapping/4-cities_by_state.py
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -1,49 +1,16 @@
 #!/usr/bin/python3
 """
-Script that lists all cities from the database hbtn_0e_4_usa.
+Lists all cities of the database hbtn_0e_4_usa, ordered by city id.
 """
-
 import sys
 import MySQLdb
 
 if __name__ == "__main__":
-    # Check if all arguments are provided
-    if len(sys.argv) != 4:
-        print("Usage: {} username password database".format(sys.argv[0]))
-        sys.exit(1)
-
-    # Retrieve username, password, and database name from command line arguments
-    username = sys.argv[1]
-    password = sys.argv[2]
-    database = sys.argv[3]
-
-    # Connect to MySQL database
-    try:
-        db = MySQLdb.connect(host="localhost", port=3306,
-                             user=username, passwd=password, db=database)
-        cursor = db.cursor()
-
-        # Execute query to fetch cities with their corresponding state names
-        cursor.execute("""
-            SELECT cities.id, cities.name, states.name
-            FROM cities
-            JOIN states ON cities.state_id = states.id
-            ORDER BY cities.id ASC
-        """)
-
-        # Fetch all rows from the query result
-        rows = cursor.fetchall()
-
-        # Print results in the required format
-        for row in rows:
-            print(row)
-
-        # Close cursor and database connection
-        cursor.close()
-        db.close()
-
-    except MySQLdb.Error as e:
-        # Handle MySQL errors
-        print("MySQL Error {}: {}".format(e.args[0], e.args[1]))
-        sys.exit(1)
-
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    c = db.cursor()
+    c.execute("SELECT `c`.`id`, `c`.`name`, `s`.`name` \
+                 FROM `cities` as `c` \
+                INNER JOIN `states` as `s` \
+                   ON `c`.`state_id` = `s`.`id` \
+                ORDER BY `c`.`id`")
+    [print(city) for city in c.fetchall()]
