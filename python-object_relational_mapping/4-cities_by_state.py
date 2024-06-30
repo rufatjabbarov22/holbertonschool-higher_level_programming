5b47c01613b1c7a0806cb1c84e071c0550f0e4b2
Rufat Jabbarov <152645357+rufatjabbarov22@users.noreply.github.com>
2024-06-30 16:40:47 +0400

    4-cities_by_state.py

--- python-object_relational_mapping/4-cities_by_state.py
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -1,16 +1,46 @@
 #!/usr/bin/python3
 """
-Lists all cities of the database hbtn_0e_4_usa, ordered by city id.
+Script that lists all cities from the database hbtn_0e_4_usa.
 """
+
 import sys
 import MySQLdb
 
 if __name__ == "__main__":
-    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
-    c = db.cursor()
-    c.execute("SELECT `c`.`id`, `c`.`name`, `s`.`name` \
-                 FROM `cities` as `c` \
-                INNER JOIN `states` as `s` \
-                   ON `c`.`state_id` = `s`.`id` \
-                ORDER BY `c`.`id`")
-    [print(city) for city in c.fetchall()]
+    # Check if all arguments are provided
+    if len(sys.argv) != 4:
+        print("Usage: {} username password database".format(sys.argv[0]))
+        sys.exit(1)
+
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+
+    # Connect to MySQL database
+    try:
+        db = MySQLdb.connect(host="localhost", port=3306,
+                             user=username, passwd=password, db=database)
+        cursor = db.cursor()
+
+        # Execute the query to fetch cities with their states
+        cursor.execute("""
+            SELECT cities.id, cities.name, states.name
+            FROM cities
+            JOIN states ON cities.state_id = states.id
+            ORDER BY cities.id ASC
+        """)
+
+        # Fetch all rows
+        rows = cursor.fetchall()
+
+        # Print results as per the example format
+        for row in rows:
+            print(row)
+
+        # Close cursor and database connection
+        cursor.close()
+        db.close()
+
+    except MySQLdb.Error as e:
+        print("MySQL Error {}: {}".format(e.args[0], e.args[1]))
+        sys.exit(1)
