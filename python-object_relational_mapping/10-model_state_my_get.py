bf630426c051af92d0b6fbb4ae3e2ec8783eed76
Rufat Jabbarov <152645357+rufatjabbarov22@users.noreply.github.com>
2024-06-30 19:19:49 +0400

    10-model_state_my_get.py

--- python-object_relational_mapping/10-model_state_my_get.py
+++ python-object_relational_mapping/10-model_state_my_get.py
@@ -1,52 +1,25 @@
 #!/usr/bin/python3
 """
-Script that prints the State object with the name passed as an argument from the database hbtn_0e_6_usa.
+Lists the State object with the name passed as argument
+from the database hbtn_0e_6_usa
 """
-
 import sys
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from model_state import Base, State
-
-def get_state_id(username, password, database, state_name):
-    try:
-
-        engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
-                               .format(username, password, database),
-                               pool_pre_ping=True)
-        Base.metadata.create_all(engine)
-
-
-        Session = sessionmaker(bind=engine)
-        session = Session()
-
-
-        state = session.query(State).filter_by(name=state_name).first()
-
-
-        if state:
-            print(state.id)
-        else:
-            print("Not found")
-
-
-        session.close()
-
-    except Exception as e:
-        print("Error:", e)
-        sys.exit(1)
+from model_state import State
 
 if __name__ == "__main__":
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
+    Session = sessionmaker(bind=engine)
+    session = Session()
 
-    if len(sys.argv) != 5:
-        print("Usage: {} username password database state_name".format(sys.argv[0]))
-        sys.exit(1)
-
-    username = sys.argv[1]
-    password = sys.argv[2]
-    database = sys.argv[3]
-    state_name = sys.argv[4]
-
-
-    get_state_id(username, password, database, state_name)
-
+    found = False
+    for state in session.query(State):
+        if state.name == sys.argv[4]:
+            print("{}".format(state.id))
+            found = True
+            break
+    if found is False:
+        print("Not found")
