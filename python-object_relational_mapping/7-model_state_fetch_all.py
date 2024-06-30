3fd170c3530ca1f5abc1f1cb31b9c75de8956187
Rufat Jabbarov <152645357+rufatjabbarov22@users.noreply.github.com>
2024-06-30 19:13:13 +0400

    7-model_state_fetch_all.py

--- python-object_relational_mapping/7-model_state_fetch_all.py
+++ python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,27 +1,18 @@
 #!/usr/bin/python3
 """
-Script that lists all State objects from the database hbtn_0e_6_usa
+Lists all State objects from the database hbtn_0e_6_usa.
 """
-
-
 import sys
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from model_state import Base, State
+from model_state import State
 
 if __name__ == "__main__":
-    usr = sys.argv[1]
-    pwd = sys.argv[2]
-    db_name = sys.argv[3]
-
-    engine = create_engine("mysql+mysqldb://{}:{}@localhost:3306/{}"
-                           .format(usr, pwd, db_name))
-
-    Base.metadata.create_all(engine)
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
     Session = sessionmaker(bind=engine)
     session = Session()
-    states = session.query(State).order_by(State.id).all()
 
-    for state in states:
-        print(f"{state.id}: {state.name}")
-    session.close()
+    for state in session.query(State).order_by(State.id):
+        print("{}: {}".format(state.id, state.name))
