54895750066c453e775a0a4d991ff04759e42156
rufatjabbarov22 <rufatjabbarov001@gmail.com>
2024-06-30 13:16:46 +0000

    task-2

---
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python3
+"""
+Script that displays all values in the states table of hbtn_0e_0_usa where name matches the argument.
+"""
+
+import sys
+import MySQLdb
+
+if __name__ == "__main__":
+
+    if len(sys.argv) != 5:
+        print("Usage: {} username password database state_name".format(sys.argv[0]))
+        sys.exit(1)
+
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+    state_name = sys.argv[4]
+
+
+    try:
+        db = MySQLdb.connect(host="localhost", port=3306,
+                             user=username, passwd=password, db=database)
+        cursor = db.cursor()
+
+
+        cursor.execute("""
+            SELECT * FROM states
+            WHERE name = %s
+            ORDER BY id ASC
+        """, (state_name,))
+
+
+        rows = cursor.fetchall()
+
+
+        for row in rows:
+            print(row)
+
+
+        cursor.close()
+        db.close()
+
+    except MySQLdb.Error as e:
+        print("MySQL Error {}: {}".format(e.args[0], e.args[1]))
+        sys.exit(1)
+
