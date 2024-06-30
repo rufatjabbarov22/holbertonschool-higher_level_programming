26217f35bada192e3a4712ab60fbabfab8e78a3d
Rufat Jabbarov <152645357+rufatjabbarov22@users.noreply.github.com>
2024-06-30 16:32:04 +0400

    model_state.py

---
+++ python-object_relational_mapping/model_state.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+Defines a State model.
+Inherits from SQLAlchemy Base and links to the MySQL table states
+"""
+
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+
+class State(Base):
+    """
+    Represents a state for a MySQL database
+    """
+    __tablename__ = "states"
+    id = Column(Integer, primary_key=True)
+    name = Column(String(128), nullable=False)
