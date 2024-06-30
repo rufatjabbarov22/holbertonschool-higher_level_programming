a0d49e9e9f0a92f2607ec2988ebe4019609c4ccf
rufatjabbarov22 <rufatjabbarov001@gmail.com>
2024-06-30 13:51:57 +0000

    100

--- python-object_relational_mapping/100-relationship_states_cities.py
+++ python-object_relational_mapping/100-relationship_states_cities.py
@@ -1,21 +1,54 @@
 #!/usr/bin/python3
 """
-Creates the State “California” with the City “San Francisco”
+Script that creates the State “California” with the City “San Francisco”
 from the database hbtn_0e_100_usa
 """
 import sys
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from relationship_state import State
-from relationship_city import Base, City
+from relationship_state import Base, State
+from relationship_city import City
+
+def create_state_city(username, password, database):
+    try:
+        engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
+                               .format(username, password, database),
+                               pool_pre_ping=True)
+        Base.metadata.create_all(engine)
+
+
+        Session = sessionmaker(bind=engine)
+        session = Session()
+
+
+        california = State(name="California")
+
+
+        san_francisco = City(name="San Francisco", state=california)
+
+
+        session.add(san_francisco)
+
+
+        session.commit()
+
+
+        session.close()
+
+    except Exception as e:
+        print("Error:", e)
+        sys.exit(1)
 
 if __name__ == "__main__":
-    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
-                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
-                           pool_pre_ping=True)
-    Base.metadata.create_all(engine)
-    Session = sessionmaker(bind=engine)
-    session = Session()
 
-    session.add(City(name="San Francisco", state=State(name="California")))
-    session.commit()
+    if len(sys.argv) != 4:
+        print("Usage: {} username password database".format(sys.argv[0]))
+        sys.exit(1)
+
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+
+
+    create_state_city(username, password, database)
+
--- python-object_relational_mapping/relationship_state.py
+++ python-object_relational_mapping/relationship_state.py
@@ -1,20 +1,17 @@
 #!/usr/bin/python3
 """
-Defines a State model.
-Inherits from SQLAlchemy Base and links to the MySQL table states
+Module that contains the State class
 """
 from sqlalchemy import Column, Integer, String
-from sqlalchemy.orm import relationship
 from sqlalchemy.ext.declarative import declarative_base
-from relationship_city import Base, City
+from sqlalchemy.orm import relationship
 
+Base = declarative_base()
 
 class State(Base):
-    """
-    Represents a state for a MySQL database.
-    """
-    __tablename__ = "states"
-    id = Column(Integer, primary_key=True)
+    """ Class representation of states table """
+    __tablename__ = 'states'
+    id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
     name = Column(String(128), nullable=False)
-
     cities = relationship("City", backref="state", cascade="all, delete")
+
