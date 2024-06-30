916ab20e53d462138f3ecdb5e818c21f2eda105e
rufatjabbarov22 <rufatjabbarov001@gmail.com>
2024-06-30 13:42:02 +0000

    task-13

--- python-object_relational_mapping/13-model_state_delete_a.py
+++ python-object_relational_mapping/13-model_state_delete_a.py
@@ -1,24 +1,52 @@
 #!/usr/bin/python3
 """
-Deletes all State objects with a name containing
-the letter a from the database hbtn_0e_6_usa.
-Usage: ./13-model_state_delete_a.py <mysql username> /
-                                    <mysql password> /
-                                    <database name>
+Script that deletes all State objects with a name containing the letter 'a'
+from the database hbtn_0e_6_usa.
 """
+
 import sys
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from model_state import State
+from model_state import Base, State
+
+def delete_states_with_a(username, password, database):
+    try:
+
+        engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
+                               .format(username, password, database),
+                               pool_pre_ping=True)
+        Base.metadata.create_all(engine)
+
+        Session = sessionmaker(bind=engine)
+        session = Session()
+
+
+        states_with_a = session.query(State).filter(State.name.like('%a%')).all()
+
+
+        for state in states_with_a:
+            session.delete(state)
+
+
+        session.commit()
+
+
+        session.close()
+
+    except Exception as e:
+        print("Error:", e)
+        sys.exit(1)
 
 if __name__ == "__main__":
-    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
-                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
-                           pool_pre_ping=True)
-    Session = sessionmaker(bind=engine)
-    session = Session()
 
-    for state in session.query(State):
-        if "a" in state.name:
-            session.delete(state)
-    session.commit()
+    if len(sys.argv) != 4:
+        print("Usage: {} username password database".format(sys.argv[0]))
+        sys.exit(1)
+
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+
+
+    delete_states_with_a(username, password, database)
+
