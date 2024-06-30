a4411eeed6ac54536a0403c28c2ffb96f1bdb9d1
Rufat Jabbarov <152645357+rufatjabbarov22@users.noreply.github.com>
2024-06-30 19:21:02 +0400

    12-model_state_update_id_2.py

--- python-object_relational_mapping/12-model_state_update_id_2.py
+++ python-object_relational_mapping/12-model_state_update_id_2.py
@@ -1,53 +1,20 @@
 #!/usr/bin/python3
 """
-Script that changes the name of a State object from the database hbtn_0e_6_usa.
+Changes the name of the State object with id = 2 to
+New Mexico in the database hbtn_0e_6_usa
 """
-
 import sys
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from model_state import Base, State
-
-def update_state(username, password, database):
-    try:
-
-        engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
-                               .format(username, password, database),
-                               pool_pre_ping=True)
-        Base.metadata.create_all(engine)
-
-
-        Session = sessionmaker(bind=engine)
-        session = Session()
-
-
-        state_to_update = session.query(State).filter_by(id=2).first()
-
-
-        if state_to_update:
-
-            state_to_update.name = "New Mexico"
-
-
-            session.commit()
-
-
-        session.close()
-
-    except Exception as e:
-        print("Error:", e)
-        sys.exit(1)
+from model_state import State
 
 if __name__ == "__main__":
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
+    Session = sessionmaker(bind=engine)
+    session = Session()
 
-    if len(sys.argv) != 4:
-        print("Usage: {} username password database".format(sys.argv[0]))
-        sys.exit(1)
-
-    username = sys.argv[1]
-    password = sys.argv[2]
-    database = sys.argv[3]
-
-
-    update_state(username, password, database)
-
+    state = session.query(State).filter_by(id=2).first()
+    state.name = "New Mexico"
+    session.commit()
