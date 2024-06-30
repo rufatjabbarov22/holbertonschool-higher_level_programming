5f1bc28e690b8c6730d0a4714b12e636185cbb49
Rufat Jabbarov <152645357+rufatjabbarov22@users.noreply.github.com>
2024-06-30 19:17:31 +0400

    8-model_state_fetch_first.py

--- python-object_relational_mapping/8-model_state_fetch_first.py
+++ python-object_relational_mapping/8-model_state_fetch_first.py
@@ -1,51 +1,21 @@
 #!/usr/bin/python3
 """
-Script that prints the first State object from the database hbtn_0e_6_usa.
+Prints the first State object from the database hbtn_0e_6_usa
 """
-
 import sys
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from model_state import Base, State
-
-def fetch_first_state(username, password, database):
-    try:
-
-        engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
-                               .format(username, password, database),
-                               pool_pre_ping=True)
-        Base.metadata.create_all(engine)
-
-
-        Session = sessionmaker(bind=engine)
-        session = Session()
-
-
-        first_state = session.query(State).order_by(State.id).first()
-
-
-        if first_state:
-            print("{}: {}".format(first_state.id, first_state.name))
-        else:
-            print("Nothing")
-
-
-        session.close()
-
-    except Exception as e:
-        print("Error:", e)
-        sys.exit(1)
+from model_state import State
 
 if __name__ == "__main__":
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
+    Session = sessionmaker(bind=engine)
+    session = Session()
 
-    if len(sys.argv) != 4:
-        print("Usage: {} username password database".format(sys.argv[0]))
-        sys.exit(1)
-
-    username = sys.argv[1]
-    password = sys.argv[2]
-    database = sys.argv[3]
-
-
-    fetch_first_state(username, password, database)
-
+    state = session.query(State).order_by(State.id).first()
+    if state is None:
+        print("Nothing")
+    else:
+        print("{}: {}".format(state.id, state.name))
