808095ad6ab1f314df957ab74e8586a4ebeeecd6
rufatjabbarov22 <rufatjabbarov001@gmail.com>
2024-06-30 13:36:02 +0000

    task-11

--- python-object_relational_mapping/11-model_state_insert.py
+++ python-object_relational_mapping/11-model_state_insert.py
@@ -1,20 +1,52 @@
 #!/usr/bin/python3
 """
-Adds the State object "Louisiana" to the database hbtn_0e_6_usa
+Script that adds the State object "Louisiana" to the database hbtn_0e_6_usa.
 """
+
 import sys
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from model_state import State
+from model_state import Base, State
+
+def add_state(username, password, database):
+    try:
+
+        engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
+                               .format(username, password, database),
+                               pool_pre_ping=True)
+        Base.metadata.create_all(engine)
+
+
+        Session = sessionmaker(bind=engine)
+        session = Session()
+
+        new_state = State(name="Louisiana")
+
+
+        session.add(new_state)
+        session.commit()
+
+
+        print(new_state.id)
+
+
+        session.close()
+
+    except Exception as e:
+        print("Error:", e)
+        sys.exit(1)
 
 if __name__ == "__main__":
-    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
-                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
-                           pool_pre_ping=True)
-    Session = sessionmaker(bind=engine)
-    session = Session()
 
-    louisiana = State(name="Louisiana")
-    session.add(louisiana)
-    session.commit()
-    print(louisiana.id)
+    if len(sys.argv) != 4:
+        print("Usage: {} username password database".format(sys.argv[0]))
+        sys.exit(1)
+
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+
+
+    add_state(username, password, database)
+
+
