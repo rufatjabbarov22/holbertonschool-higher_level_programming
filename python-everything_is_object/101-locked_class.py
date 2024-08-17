e91105973009e29b8c401fc922ca9bd2444b340c
Rufat Jabbarov <152645357+rufatjabbarov22@users.noreply.github.com>
2024-08-17 18:50:21 +0400

    101-locked_class.py

---
+++ python-everything_is_object/101-locked_class.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+Script that contains LockedClass class
+"""
+
+
+class LockedClass:
+    """
+    a class LockedClass with no class or object attribute
+    """
+    __slots__ = ['first_name']
